6cb9e1086d6ea29c46ee74565d5d402cd117a867
Melissa M. Dailey <mel.dailey@gmail.com>
2015-12-14 15:52:44 -0800

    partial lightbox, not working in underscore

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
 
 
 $(document).ready(function (){  
-
 	var url = "http://d345h07ts0fu2m.cloudfront.net/379/data48.json";
 	var eventTemplate = _.template($('#event-template').html());
-	 
-	var success = function(data){
-		var eventsEn2015 = data.categories.EnglishEvents2015.entries;
-		
-		eventsEn2015.forEach(function(element) {
+	
+	var success = function(data){	
+		var events2015 = data.categories.EnglishEvents2015.entries;
+		events2015.forEach(function(element) {
 			var eventHtml = eventTemplate(element);
 			$("#events-placeholder").append(eventHtml);
-
 		});
-	
-
 	};
+
+	$("a#show-panel").click(function(){
+		console.log("this is working")
+		$("#lightbox, #lightbox-panel").fadeIn(300);
+	});
+	
+	$("a#close-panel").click(function(){
+		$("#lightbox, #lightbox-panel").fadeOut(300);
+	})
 	 
-	$.ajax({
+	$.ajax({	
 	  type: 'GET',    
 	  url: url,
 	  async: false,
