e80ca622edcbefbb6e062e20317f053a011e4c96
Melissa M. Dailey <mel.dailey@gmail.com>
2015-12-14 20:24:18 -0800

    lightbox functionality is at 8 out of 10

--- js/app.js
+++ js/app.js
@@ -2,25 +2,39 @@
 
 $(document).ready(function (){  
 	var url = "http://d345h07ts0fu2m.cloudfront.net/379/data48.json";
+	var entryId;
+
+	// Templates
 	var eventTemplate = _.template($('#event-template').html());
+
 	
+	// Gather data from endpoint
 	var success = function(data){	
 		var events2015 = data.categories.EnglishEvents2015.entries;
+		entryId = events2015.entry_id;
+		
 		events2015.forEach(function(element) {
+			
+			// events main view
 			var eventHtml = eventTemplate(element);
 			$("#events-placeholder").append(eventHtml);
+
 		});
 	};
+    
+    // Open lightboxs
+    $("#events-placeholder").on("click", ".show-panel", function(e) {
+    	var entry_id=$(this).attr("data-id");
+    	console.log(entry_id);
+		$("#lightbox-panel-"+entry_id).fadeIn(300);
+    });
 
-	$("a#show-panel").click(function(){
-		console.log("this is working")
-		$("#lightbox, #lightbox-panel").fadeIn(300);
-	});
-	
-	$("a#close-panel").click(function(){
-		$("#lightbox, #lightbox-panel").fadeOut(300);
-	})
-	 
+    //Close lightbox
+    $("#events-placeholder").on("click", "#close-panel", function(e) {
+		$(".lightbox-panel").fadeOut(300);
+    });
+
+	 // retrieve data from endpoint in jsonp form
 	$.ajax({	
 	  type: 'GET',    
 	  url: url,
